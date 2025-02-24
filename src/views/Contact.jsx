2448ce30e2013da5cd7f24ff820accc72e4fddee
dyueskinner <dyueskinner@gmail.com>
2025-02-24 09:32:03 -0400

    added contact information

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -5,7 +5,18 @@ const Contact = () => {
         <section id="contact">
 
             <div className="bg-[#1A1A1A]">
-                <h1 className="text-[#FFAE00] text-center pt-10">Contact</h1>
+                <h1 className="text-[#FFAE00] text-[24px] underline text-center pt-10">Contact Me</h1>
+                <p className="text-white px-12 text-center">Thank you for taking the time to take a look at my portfolio. If you would like to reach out I can be reached at:</p>
+                <br />
+                <p className="text-white text-center pb-10">
+                    Dominique Yue-Skinner
+                    <br />
+                    <a className="underline text-[#FFAE00] hover:text-white" href="mailto:dyueskinner@gmail.com">dyueskinner@gmail.com</a>
+                    <br />
+                    782-446-7022
+                    <br />
+                    <a className="underline text-[#FFAE00] hover:text-white" href="https://www.linkedin.com/in/dyueskinner">LinkedIn Profile</a>
+                </p>
             </div>
 
 
