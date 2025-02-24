457871163b82a39fb6fb9bcde86df1ecd6953ad6
dyueskinner <dyueskinner@gmail.com>
2025-02-24 09:32:30 -0400

    changed year in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@
 export default function Footer() {
     return(
         <footer className='bg-[#242424] text-white text-center pt-4 pb-4'>
-            <p>&copy; 2021 Dominique Yue-Skinner | <span className="text-[#FFAE00]">Built using React.js</span></p>
+            <p>&copy; 2025 Dominique Yue-Skinner | <span className="text-[#FFAE00]">Built using React.js</span></p>
         </footer>
     )
 }
